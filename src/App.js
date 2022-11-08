1cf0ef2318034bb0dee26100254120f4c57c3134
NeyamulOmy <neyamul.omy@gmail.com>
2022-11-09 00:26:05 +0600

    setup router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './components/Main';
+import Home from './components/Home';
 
 function App() {
+  const routes = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+          {
+            path: '/',
+            element: <Home></Home>
+          }
+        ]
+      }
+    ]
+  )
   return (
     <div className="App">
-      Hello world
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
