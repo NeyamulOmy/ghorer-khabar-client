c7e2547faeadd9172f0139a9cc53b923ac132bff
NeyamulOmy <neyamul.omy@gmail.com>
2022-11-08 22:18:02 +0600

    added firebase.config.js

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDSCiDYmOLh8-7ssaXRNTwgGNpF-GfrXRo",
+    authDomain: "ghorer-khabar-auth.firebaseapp.com",
+    projectId: "ghorer-khabar-auth",
+    storageBucket: "ghorer-khabar-auth.appspot.com",
+    messagingSenderId: "427675016145",
+    appId: "1:427675016145:web:6c1e414d788ef6d869def0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
