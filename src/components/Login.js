55ab532c21525fbd4fade4e4e9aa7d2b62cebbaf
NeyamulOmy <neyamul.omy@gmail.com>
2022-11-11 17:17:19 +0600

    added jwt for social login

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,10 +21,28 @@ const Login = () => {
     const handleGoogle = () => {
         handleGoogleSignIn(Googleprovider)
             .then((result) => {
-
-
                 const user = result.user;
-                navigate(from, { replace: true });
+                console.log(user);
+                const currentUser = {
+                    email: user.email
+                }
+                console.log(currentUser);
+                //get jwt token
+                fetch('https://ghorer-khabar-server.vercel.app/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(currentUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        localStorage.setItem('token', data.token);
+                        navigate(from, { replace: true });
+                    })
+
+
             }).catch((error) => {
                 console.log("error : ", error);
             })
@@ -59,9 +77,10 @@ const Login = () => {
                     .then(data => {
                         console.log(data);
                         localStorage.setItem('token', data.token);
+                        navigate(from, { replace: true });
                     })
 
-                navigate(from, { replace: true });
+
 
 
             })
